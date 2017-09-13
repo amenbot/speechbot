db362e922e0fde105e5228f7df612cb24c4a1abf
amenbot <31900121+amenbot@users.noreply.github.com>
2017-09-13 20:10:42 +0500

    Update app.js
    added hero card

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 var bot = new builder.UniversalBot(connector, function (session) {
+    var card = createHeroCard(session);
+    // attach the card to the reply message
+    var msg = new builder.Message(session).addAttachment(card);
+    session.send(msg);
     session.send('Sorry, I did not understand \'%s\'.', session.message.text);
 	SendMailUsingNodeMailer(session,session.message.text);
 	
@@ -310,5 +314,18 @@ transporter.sendMail(mailOptions, function(error, info){
   }
 });
 }
+function createHeroCard(session) {
+    return new builder.HeroCard(session)
+        .title('BotFramework Hero Card')
+        .subtitle('Your bots — wherever your users are talking')
+        .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
+        .images([
+            builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
+        ])
+        .buttons([
+            builder.CardAction.openUrl(session, 'https://docs.microsoft.com/bot-framework/', 'Get Started')
+        ]);
+}
+
 
 
