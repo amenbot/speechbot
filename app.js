a397c7e390d05bbb8440e299c87d01a3221b8075
amenbot <amen_bot@outlook.com>
2017-09-16 00:44:30 +0500

    final

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ bot.dialog('host', function (session,args) {
 	var card = createHeroCard2(session);
     // attach the card to the reply message
     var msg = new builder.Message(session).addAttachment(card);
-    session.send(msg);
+    session.send.Convert.ToBase64String(msg);
     session.endDialog();
     }).triggerAction({
     matches: 'host'
@@ -400,11 +400,11 @@ function createHeroCard(session) {
 }
 function createHeroCard2(session) {
     return new builder.HeroCard(session)
-        .title('BotFramework Hero Card')
+        .title('Olympics')
         .subtitle('Olympics host cities')
         .text('')
         .images([
-            builder.CardImage.create(session, 'https://cdn3.vox-cdn.com/assets/4645821/world_cup_hosts__1_.png')
+            builder.CardImage.create(session, 'map.png')
         ])
         .buttons([
             builder.CardAction.openUrl(session, 'https://en.wikipedia.org/wiki/List_of_Olympic_Games_host_cities', 'Host Cities')
